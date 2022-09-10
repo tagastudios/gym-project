9735f7e376dbe66d0c54c7971a8c9be4217a1a69
Taga Studios <tagastudios@gmail.com>
2022-09-10 14:03:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./views/Home";
+import Amenities from "./views/Amenities";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="amenities" element={<Amenities />} />
+				{/* <Route path="contact" element={<ContactPage />} />
+				<Route path="about" element={<AboutPage />} />
+				<Route path="directory" element={<CampsitesDirectoryPage />} />
+				<Route path="directory/:campsiteId" element={<CampsiteDetailPage />} /> */}
+			</Routes>
+			{/* <Footer /> */}
+		</div>
+	);
 }
 
 export default App;
